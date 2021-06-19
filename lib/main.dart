ebfc9b14061c8a77770674a067f6a7ac858891b1
Nathaliaaw <natalia4cardozo@gmail.com>
2021-06-18 22:17:15 -0500

    Cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
  //@dart=2.9
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:peliculas/src/pages/home_pages.dart';
 void main() => runApp(MyApp());
  
